1f860d3f66be31c36cc1026a55d89e3cdca788dc
alexanderwiederin <alexander.wiederin@gmail.com>
2018-12-05 11:25:10 -0800

    challenge_2 set up dev environment(eslint) and server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
 
+const PORT = 3000;
 
+const app = express();
+
+app.get('/v1/bpi/historical/close.json', proxy({ target: 'https://api.coindesk.com/', changeOrigin: true }));
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
