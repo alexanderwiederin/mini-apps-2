6e3b1d12d8f5ba71f463d535b2199a7a6cf0969d
alexanderwiederin <alexander.wiederin@gmail.com>
2018-12-06 11:50:50 -0800

    setup client get request

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bpi: {},
+      labels: [],
+      data: [],
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('/v1/bpi/historical/close.json')
+      .then((results) => {
+        const { bpi } = results.data;
+        const labels = Object.keys(bpi);
+        const data = Object.values(bpi);
+        this.setState({ bpi, labels, data });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const morgan = require('morgan');
+const path = require('path');
 
 const PORT = 3000;
 
 const app = express();
+app.use(morgan());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/v1/bpi/historical/close.json', proxy({ target: 'https://api.coindesk.com/', changeOrigin: true }));
 
